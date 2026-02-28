cf40f6df0d6f823595ed7ab3886dcfe6c3070fc0
s-Kalsi <145471139+s-Kalsi@users.noreply.github.com>
2026-02-28 13:28:47 +0530

    Change default port from 5000 to 8000

--- app.py
+++ app.py
@@ -212,5 +212,5 @@ def health():
 
 if __name__ == '__main__':
     load_data()
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port, debug=False)
